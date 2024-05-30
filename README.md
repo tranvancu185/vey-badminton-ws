b92abe72864f7e04857580bd04fedba2c0ba62b5
quytp <quytp@hasaki.vn>
2024-05-30 17:38:16 +0700

    ssave

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # vey-badminton-ws
+
 The service api of vey-badmin-ton
+
+# Web Services Package
+
+This package provides a set of web services for handling the API backend of the vey-badminton application.
+
+## Installation
+
+To install the package, you can use a package manager such as npm or yarn:
---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import express, {Express, Request, Response} from "express";
+
+const app: Express = express();
+const port = 8080;
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello, World!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port  ${port}`);
+});
