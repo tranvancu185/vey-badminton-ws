8560c581bd74059bd8732d2ee6bbfd8242bebe56
quytp <quytp@hasaki.vn>
2024-06-02 20:58:18 +0700

    save

--- src/packages/users/routes.ts
+++ src/packages/users/routes.ts
@@ -6,25 +6,85 @@ const userRouter: Router = express.Router();
 userRouter.use(authenticateToken);
 // Define your routes
 
-
+/**
+ * @swagger
+ * /:
+ *   get:
+ *     description: Get a list of users
+ *     responses:
+ *       200:
+ *         description: Success
+ */
 userRouter.get('/', async (req: Request, res: Response) => {
     const userController = new UserController();
     const respone = await userController.GetListUsers(req);
     res.json(respone)
 });
 
+/**
+ * @swagger
+ * /:id:
+ *   get:
+ *     description: Get a user by ID
+ *     parameters:
+ *       - name: id
+ *         description: User's ID
+ *         in: path
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: Success
+ */
 userRouter.get('/:id', (req: Request, res: Response) => {
     // Handle GET request for /users/:id
     const userId = req.params.id;
     res.json({ id: userId });
 });
 
+/**
+ * @swagger
+ * /create:
+ *   post:
+ *     description: Create a new user
+ *     parameters:
+ *       - name: user
+ *         description: User object
+ *         in: body
+ *         required: true
+ *         schema:
+ *           $ref: '#/definitions/User'
+ *     responses:
+ *       200:
+ *         description: Success
+ */
 userRouter.post('/create', (req: Request, res: Response) => {
     // Handle POST request for /users
     const newUser = req.body;
     res.send('Create a new user');
 });
 
+/**
+ * @swagger
+ * /:id/edit:
+ *   put:
+ *     description: Update a user by ID
+ *     parameters:
+ *       - name: id
+ *         description: User's ID
+ *         in: path
+ *         required: true
+ *         type: string
+ *       - name: user
+ *         description: User object
+ *         in: body
+ *         required: true
+ *         schema:
+ *           $ref: '#/definitions/User'
+ *     responses:
+ *       200:
+ *         description: Success
+ */
 userRouter.put('/:id/edit', (req: Request, res: Response) => {
     // Handle PUT request for /users/:id
     const userId = req.params.id;
@@ -32,6 +92,21 @@ userRouter.put('/:id/edit', (req: Request, res: Response) => {
     res.send(`Update user with ID ${userId}`);
 });
 
+/**
+ * @swagger
+ * /:id/delete:
+ *   delete:
+ *     description: Delete a user by ID
+ *     parameters:
+ *       - name: id
+ *         description: User's ID
+ *         in: path
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: Success
+ */
 userRouter.delete('/:id/delete', (req: Request, res: Response) => {
     // Handle DELETE request for /users/:id
     const userId = req.params.id;
